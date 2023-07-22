553872c177cf0264b2f9d2c332fc6851f24066de
sschr15 <steven.sschr15@hotmail.com>
2023-07-21 22:57:28 -0500

    1.19.4 update 1.19.3 is in the air, if someone needs specifically that and the .2 version doesn't work, create issue please thank

--- src/main/java/sschr15/fabricmods/nodoubletapsprint/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/sschr15/fabricmods/nodoubletapsprint/mixin/ClientPlayerEntityMixin.java
@@ -2,6 +2,7 @@ package sschr15.fabricmods.nodoubletapsprint.mixin;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -10,9 +11,8 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import sschr15.fabricmods.nodoubletapsprint.client.DisableDoubleTapAccessor;
 
 @Mixin(LocalPlayer.class)
-public abstract class ClientPlayerEntityMixin {
+public abstract class ClientPlayerEntityMixin extends LivingEntity {
     @Shadow protected @Final Minecraft minecraft;
-    @Shadow public abstract void setSprinting(boolean sprinting);
 
     /**
      * @reason Removes double tap sprinting (as a config option)
@@ -21,5 +21,9 @@ public abstract class ClientPlayerEntityMixin {
     private void setSprinting(LocalPlayer player, boolean sprinting) {
         DisableDoubleTapAccessor options = (DisableDoubleTapAccessor) minecraft.options;
         setSprinting(options.nodoubletap$isDoubleTapDisabled() ? options.nodoubletap$isSprintKeyPressed() : sprinting);
+    }
+
+    private ClientPlayerEntityMixin() {
+        super(null, null);
     }
 }
--- src/main/java/sschr15/fabricmods/nodoubletapsprint/mixin/ControlsOptionsScreenMixin.java
+++ src/main/java/sschr15/fabricmods/nodoubletapsprint/mixin/ControlsOptionsScreenMixin.java
@@ -3,8 +3,7 @@ package sschr15.fabricmods.nodoubletapsprint.mixin;
 import net.minecraft.client.gui.screens.OptionsSubScreen;
 import net.minecraft.client.gui.screens.controls.ControlsScreen;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import sschr15.fabricmods.nodoubletapsprint.client.DoubleTapOption;
@@ -20,6 +19,11 @@ public abstract class ControlsOptionsScreenMixin extends OptionsSubScreen {
             locals = LocalCapture.CAPTURE_FAILHARD
     )
     private void onInit(CallbackInfo ci, int i, int j, int k) {
-        addRenderableWidget(DoubleTapOption.DOUBLE_TAP_SPRINT.createButton(options, j, k, 150));
+        addRenderableWidget(DoubleTapOption.DOUBLE_TAP_SPRINT.createButton(options, i, k + 24, 150));
+    }
+
+    @ModifyVariable(method = "init", index = 3, at = @At(value = "FIELD", target = "Lnet/minecraft/network/chat/CommonComponents;GUI_DONE:Lnet/minecraft/network/chat/Component;"))
+    private int modifyHeight(int yDrawPosition) {
+        return yDrawPosition + 24;
     }
 }
